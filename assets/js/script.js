c04a3644e7bd5afdbdeb8f165dd273d9f37c794d
Caleeeb <daycinematography@gmail.com>
2022-03-19 13:31:20 -0400

    added comments to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,21 @@ let passwordLength = 8;
 
 // function that promts the user to chose 8 characters and no more than 128 characters
 let userPrompt = function () {
+  // prompt user for length of password 
   passwordLength = prompt("How long would you like you password from 8-128?");
+  // check if the length falls under the parameters set, if not restart function
   if (passwordLength <= 8 && passwordLength >= 128) {
     window.alert("You need to provide a valid answer! Please try again.");
     return userPrompt();
   }
+  // set a while loop that will restart if no character options are chosen
   while (characterPool.length == 0) {
+    // user choses what character options they want.
     let lowercase = confirm("Would you like your password to have lowercase letters?");
     let uppercase = confirm("Would you like your password to have uppercase letters?");
     let numeric = confirm("Would you like your password to have numbers?");
     let specialCharacters = confirm("Would you like your password to have special characters?");
+    // based on the user's character options selected, concatenate those arrays
     if (lowercase) {
       characterPool = characterPool.concat(letters);
     }
@@ -35,13 +40,16 @@ let userPrompt = function () {
       alert("You must chose at least one character option.");
     }
   }
+  // generate the password
   writePassword();
 }
 
 // function that generates the password to the parameters that the users has given from the prompt
 function generatePassword() {
+  // normal start of an empty string variable for output password
   let password = "";
   for (i = 0; i < passwordLength; i++) {
+    // use the random number function pull from the array cancatenated in generator function
     password = password + characterPool[randomNumber(0, characterPool.length - 1)];
   }
   return password;
@@ -54,8 +62,6 @@ var randomNumber = function (min, max) {
   return value;
 };
 
-// random number 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -63,6 +69,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
